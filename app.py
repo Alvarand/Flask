0c8220d16aa224cfda8009bf9c586a83d49ebb37
Shamil <36115557+Alvarand@users.noreply.github.com>
2021-03-16 22:43:06 +0300

    templates
    Created main template, added css

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 @app.route('/home')
 def start():
-    return render_template('base.html')
+    return render_template('home.html')
+
+
+@app.route('/blogs')
+def blogs():
+    return render_template('blogs.html')
+
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
 
 
 if __name__ == '__main__':
